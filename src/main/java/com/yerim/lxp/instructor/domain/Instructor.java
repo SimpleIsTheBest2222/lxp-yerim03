5e1887d6ba0eef45b1db767db06689063878a8af
yerim <na90303@naver.com>
2026-04-21 23:38:32 +0900

    refactor: Instructor 도메인 객체를 Rich Domain Model로 개선

--- src/main/java/com/yerim/lxp/instructor/domain/Instructor.java
+++ src/main/java/com/yerim/lxp/instructor/domain/Instructor.java
@@ -1,17 +1,51 @@
 package com.yerim.lxp.instructor.domain;
 
 public class Instructor {
+	private static final int MAXIMUM_NAME_RANGE = 10;
+	private static final int MAXIMUM_INTRODUCTION_RANGE = 100;
+
 	private Long id;
 	private String name;
 	private String introduction;
 
-	public Instructor() {
+	private Instructor() {}
+
+	public static Instructor create(String name, String introduction) {
+		Instructor instructor = new Instructor();
+		instructor.name = validateName(name);
+		instructor.introduction = validateIntroduction(introduction);
+		return instructor;
 	}
 
-	public Instructor(Long id, String name, String introduction) {
-		this.id = id;
-		this.name = name;
-		this.introduction = introduction;
+	public static Instructor createWithId(Long id, String name, String introduction) {
+		Instructor instructor = new Instructor();
+		instructor.id = id;
+		instructor.name = validateName(name);
+		instructor.introduction = validateIntroduction(introduction);
+		return instructor;
+	}
+
+	public void update(String name, String introduction) {
+		if (name != null) {
+			this.name = validateName(name);
+		}
+		if (introduction != null) {
+			this.introduction = validateIntroduction(introduction);
+		}
+	}
+
+	private static String validateName(String name) {
+		if(name == null || name.isEmpty() || name.length() > MAXIMUM_NAME_RANGE) {
+			throw new IllegalArgumentException("강사명 길이는 최대 10 입니다.");
+		}
+		return name;
+	}
+
+	private static String validateIntroduction(String introduction) {
+		if(introduction == null || introduction.isEmpty() || introduction.length() > MAXIMUM_INTRODUCTION_RANGE) {
+			throw new IllegalArgumentException("강사 설명 길이는 최대 100 입니다.");
+		}
+		return introduction;
 	}
 
 	public Long getId() {
