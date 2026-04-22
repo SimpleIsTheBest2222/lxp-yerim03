99f2a5e26bb0832ba57388809b771d58db976872
yerim <na90303@naver.com>
2026-04-22 10:44:06 +0900

    feat: InstructorService 생성 및 강사 등록 기능 구현

---
+++ src/main/java/com/yerim/lxp/instructor/service/InstructorService.java
@@ -0,0 +1,21 @@
+package com.yerim.lxp.instructor.service;
+
+import com.yerim.lxp.instructor.domain.Instructor;
+import com.yerim.lxp.instructor.dto.InstructorRequest;
+import com.yerim.lxp.instructor.repository.InMemoryInstructorRepository;
+
+public class InstructorService {
+	static InMemoryInstructorRepository instructorRepository;
+
+	public Instructor createInstructor(InstructorRequest instructorRequest) {
+		Instructor instructor = Instructor.create(instructorRequest.getName(), instructorRequest.getIntroduction());
+		return instructorRepository.save(instructor);
+	}
+
+	public void getAllInstructors() {}
+
+	public void updateInstructor() {}
+
+	public void deleteInstructor() {}
+
+}
